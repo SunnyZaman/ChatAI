f7e2f74f6906e7b699b747e647ba2a54a07ebc23
Zeeshan Chaudhry <zeeshan@Zeeshans-MacBook-Pro.local>
2023-11-02 00:30:11 -0400

    first commmit

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,8 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
+   <h1>Hello World</h1>
+     </div>
   <HelloWorld msg="Vite + Vue" />
 </template>
 
